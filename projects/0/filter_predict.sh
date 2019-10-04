4fd547fdc1b7b9b304d672b80707a9ae507da1bb
datamove <datamove@gmail.com>
2019-10-04 10:44:17 +0000

    filter and prediction in a single mapred job

---
+++ projects/0/filter_predict.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+HADOOP_CUR=/usr/hdp/current/
+HADOOP_EXE=$HADOOP_CUR/hadoop-client/bin/yarn
+HADOOP_STREAM_JAR=$HADOOP_CUR/hadoop-mapreduce-client/hadoop-streaming.jar
+FILES=$1
+INPUT=$2
+OUTPUT=$3
+MAPPER=$4
+REDUCER=$5
+$HADOOP_EXE jar $HADOOP_STREAM_JAR  -files $FILES -input $INPUT -output $OUTPUT -mapper "$MAPPER" -reducer "$REDUCER"
+
+
